3a08845b0f8f3c537674c5b0be835db1255d287a
hostei <hostei@qq.com>
2025-04-30 15:03:25 +0800

    测试9.20

--- build_wine.sh
+++ build_wine.sh
@@ -211,7 +211,8 @@ if [ -n "${CUSTOM_SRC_PATH}" ]; then
 	BUILD_NAME="${WINE_VERSION}"-custom
 elif [ "$WINE_BRANCH" = "staging-tkg" ] || [ "$WINE_BRANCH" = "staging-tkg-ntsync" ]; then
 	if [ "$WINE_BRANCH" = "staging-tkg" ] && [ "${EXPERIMENTAL_WOW64}" = "true" ]; then
-		git clone https://github.com/Kron4ek/wine-tkg wine -b wow64
+		#git clone https://github.com/Kron4ek/wine-tkg wine -b wow64
+		git clone https://github.com/hostei33/wine-tkg wine -b wine-9.20
 	else
 		if [ "$WINE_BRANCH" = "staging-tkg" ]; then
 			git clone https://github.com/Kron4ek/wine-tkg wine
