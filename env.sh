53cf3dcd3c2af297fd7c3b341d3e66b43765e0b1
Waim908 <waimxj449@foxmail.com>
2025-10-24 21:51:52 +0800

    update

---
+++ env.sh
@@ -0,0 +1,17 @@
+export CC="gcc-11"
+export CXX="g++-11"
+
+export CROSSCC_X32="i686-w64-mingw32-gcc"
+export CROSSCXX_X32="i686-w64-mingw32-g++"
+export CROSSCC_X64="x86_64-w64-mingw32-gcc"
+export CROSSCXX_X64="x86_64-w64-mingw32-g++"
+
+export CFLAGS_X32="-march=i686 -msse2 -mfpmath=sse -O3 -ftree-vectorize"
+export CFLAGS_X64="-march=x86-64 -msse3 -mfpmath=sse -O3 -ftree-vectorize"
+export LDFLAGS="-Wl,-O3,--sort-common,--as-needed"
+
+export CROSSCFLAGS_X32="${CFLAGS_X32}"
+export CROSSCFLAGS_X64="${CFLAGS_X64}"
+export CROSSLDFLAGS="${LDFLAGS}"
+
+export configure_arg=(--disable-winemenubuilder --disable-win16 --disable-tests --without-capi --without-coreaudio --without-cups --without-gphoto --without-osmesa --without-oss --without-pcap --without-pcsclite --without-sane --without-udev --without-unwind --without-usb --without-v4l2 --without-wayland --without-xinerama --without-piper)
