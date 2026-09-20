0a0d9a87a63e31b474c95303d4be6e2675a7ddf4
hostei33 <hostei33@users.noreply.github.com>
2026-09-20 23:05:31 +0800

    Enable udev support by removing --without-udev from build options

--- build_wine.sh
+++ build_wine.sh
@@ -78,7 +78,7 @@ export DO_NOT_COMPILE="false"
 # Make sure that ccache is installed before enabling this.
 export USE_CCACHE="false"
 
-export WINE_BUILD_OPTIONS="--without-bluez --without-udev --without-oss --without-tools --without-coreaudio  --without-cups --without-gphoto --without-pcsclite --without-sane  --disable-tests --without-v4l2 --without-xinerama --without-wayland --disable-debug"
+export WINE_BUILD_OPTIONS="--without-bluez --without-oss --without-tools --without-coreaudio  --without-cups --without-gphoto --without-pcsclite --without-sane  --disable-tests --without-v4l2 --without-xinerama --without-wayland --disable-debug"
 # A temporary directory where the Wine source code will be stored.
 # Do not set this variable to an existing non-empty directory!
 # This directory is removed and recreated on each script run.
